51903f92c4688e2ca15c3999e3d9a78d7a66ea0d
Igor S. <admin@vellnar.com>
2025-09-10 20:52:05 +0200

    test email

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
 
   try {
     await resend.emails.send({
-      from: 'Vellnar <noreply@vellnar.com>',
+      from: 'onboarding@resend.dev',
       to: [email],
       subject: 'Benvenuto nella newsletter di Vellnar!',
       html: `
@@ -45,7 +45,7 @@ export default async function handler(req, res) {
     })
 
     await resend.emails.send({
-      from: 'Vellnar <noreply@vellnar.com>',
+      from: 'onboarding@resend.dev',
       to: ['admin@vellnar.com'],
       subject: 'Nuova iscrizione newsletter',
       html: `
